91c5f5d1d73642387318a4e8fa1dc59c44bb6802
Muhammad 'Amrudien <muh.amrudien@gmail.com>
2023-03-07 16:39:03 +0700

    Create and init auth directory

---
+++ auth/src/main/kotlin/id/ac/ugm/coffeeshop/auth/AuthApplication.kt
@@ -0,0 +1,11 @@
+package id.ac.ugm.coffeeshop.auth
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AuthApplication
+
+fun main(args: Array<String>) {
+	runApplication<AuthApplication>(*args)
+}
