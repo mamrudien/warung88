397ce00c3470d4a1be6ad00572e1402d6f917757
Muhammad 'Amrudien <muh.amrudien@gmail.com>
2023-03-09 02:23:21 +0700

    Enable Gradle BuildScan

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,18 @@
+plugins {
+    id("com.gradle.enterprise") version("3.9")
+}
+
 rootProject.name = "coffeeShop"
 
 include("auth")
+
+
+gradleEnterprise {
+    if (System.getenv("CI") != null) {
+        buildScan {
+            publishAlways()
+            termsOfServiceUrl = "https://gradle.com/terms-of-service"
+            termsOfServiceAgree = "yes"
+        }
+    }
+}
